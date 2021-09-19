a0d38b87f00d49733fb71fa4a62fa106273d600d
Kirill Galkin <malo2dis@gmail.com>
2021-09-19 22:14:47 +0700

    add game card stylization

--- src/app/components/game-card/game-card.component.ts
+++ src/app/components/game-card/game-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Word } from 'src/app/app.model';
+import { Utils } from 'src/app/utils';
 
 @Component({
   selector: 'app-game-card',
@@ -13,9 +14,14 @@ export class GameCardComponent implements OnInit {
 
   @Output() onCheckCard: EventEmitter<boolean> =  new EventEmitter<boolean>();
 
+  public cardLanguage: "en" | "ru";
+
   constructor() { }
 
   ngOnInit() {
+    this.cardLanguage = Utils.reverseLanguage(this.currentLanguage);
   }
+
+
 
 }
--- src/app/utils.ts
+++ src/app/utils.ts
@@ -2,4 +2,8 @@ export class Utils {
   static getRandomNumber(maxNum: number): number {
     return Math.floor(Math.random() * maxNum);
   }
+
+  static reverseLanguage(currentLanguage: "en" | "ru"): "en" | "ru" {
+    return currentLanguage === "en" ? "ru" : "en";
+  }
 }
