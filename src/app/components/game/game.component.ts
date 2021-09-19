ca4c82f02c2e0264466dd0ca2e49731bc7636569
Kirill Galkin <malo2dis@gmail.com>
2021-09-19 19:44:33 +0700

    small fix

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
+  public currentWord: string = 'Hello';
+
   constructor() { }
 
   ngOnInit() {
